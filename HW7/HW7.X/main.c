1808cf45b7b313f887c627c3e2d058faa9d83a19
Yingfen Yi <32470088+YingfenYi@users.noreply.github.com>
2018-05-16 19:33:15 -0500

    hw7

--- HW7/HW7.X/main.c
+++ HW7/HW7.X/main.c
@@ -7,63 +7,43 @@
 
 
 #define SLAVE_ADDR 0x6B  //0b 110 1011
+#define OUT_TEMP_L 
 
 void initIMU();
-//write to the expander
-void setIMU(char pin, char level);
-//read from the expander
-char getExpander();
+void I2C_read_multiple(unsigned char address, unsigned char register, unsigned char * data, int length);
+
 
 int main() {
 // some initialization function to set the right speed setting
   __builtin_disable_interrupts();
+  /*
   TRISAbits.TRISA4 = 0; // set LED an output pin
   TRISBbits.TRISB4 = 1; // set push button an input pin
   LATAbits.LATA4 = 0; // turn LED off
-  initExpander();
-  setExpander(0,1);
+  */
+  initIMU();
   __builtin_enable_interrupts();
   char level;
   while(1) {
-    //indicating LED
-    while(_CP0_GET_COUNT() < 2400000){;}  // 24MHz/10Hz = 2400000
-    LATAINV = 0x10;     // turn off/on LED
-    _CP0_SET_COUNT(0);  // Reset the timer
-    while(!PORTBbits.RB4){;} // if button is pushed, stop and wait
-    //I2C
-    level = (getExpander()>>7);
-    setExpander(0,!level);
+
   }
   return 0;
 }
 
-void initExpander(){
+void initIMU(){
   ANSELBbits.ANSB2 = 0;
   ANSELBbits.ANSB3 = 0;
   i2c_master_setup();
   i2c_master_start(); // make the start bit
   i2c_master_send(SLAVE_ADDR<<1|0); // 0 indicate writing
-  i2c_master_send(0x00); // write to IODIR
-  i2c_master_send(0xF0); // GP7-GP4:read; GP3-GP0:write
+  i2c_master_send(0x10); // write to CTRL1_XL
+  i2c_master_send(0x82); // 0b 1000 00 10: 1.66 kHz, with 2g sensitivity, and 100 Hz filter.
+  i2c_master_send(0x11); // write to CTRL2_G
+  i2c_master_send(0x88); // 0b 1000 10 0 0: 1.66 kHz, with 1000 dps sensitivity.
+  i2c_master_send(0x12); // write to CTRL3_C
+  i2c_master_send(0x04); // 0b 0000 0100: Enable IF_INC
   i2c_master_stop(); // make the stop bit
 }
+void I2C_read_multiple(unsigned char address, unsigned char register, unsigned char * data, int length){
 
-void setExpander(char pin, char level){
-  i2c_master_start(); // make the start bit
-  i2c_master_send(SLAVE_ADDR<<1|0); // 0 indicate writing
-  i2c_master_send(0x0A); // write to OLAT
-  i2c_master_send(level<<pin); // the value to put in the register
-  i2c_master_stop(); // make the stop bit
-}
-
-char getExpander(){
-  i2c_master_start(); // make the start bit
-  i2c_master_send(SLAVE_ADDR<<1|0); // 0 indicate writing
-  i2c_master_send(0x09); // send the register address of GPIO
-  i2c_master_restart(); // make the restart bit
-  i2c_master_send(SLAVE_ADDR<<1|1); // 1 indicate reading
-  char r = i2c_master_recv(); // save the value returned
-  i2c_master_ack(1); // make the ack so the slave knows we got it
-  i2c_master_stop(); // make the stop bit
-  return r;
 }
