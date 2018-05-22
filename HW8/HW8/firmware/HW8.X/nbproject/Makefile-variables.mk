4726bdd59fa28e2e23a2e73cca43b5c4418043ac
Yingfen Yi <32470088+YingfenYi@users.noreply.github.com>
2018-05-21 20:50:59 -0500

    hw8

--- HW8/HW8/firmware/HW8.X/nbproject/Makefile-variables.mk
+++ HW8/HW8/firmware/HW8.X/nbproject/Makefile-variables.mk
@@ -4,10 +4,10 @@
 # NOCDDL
 #
 CND_BASEDIR=`pwd`
-# default configuration
-CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=HW8.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/HW8.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=hw8.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hw8.x.tar
+# PIC32MX250F128B configuration
+CND_ARTIFACT_DIR_PIC32MX250F128B=dist/PIC32MX250F128B/production
+CND_ARTIFACT_NAME_PIC32MX250F128B=HW8.X.production.hex
+CND_ARTIFACT_PATH_PIC32MX250F128B=dist/PIC32MX250F128B/production/HW8.X.production.hex
+CND_PACKAGE_DIR_PIC32MX250F128B=${CND_DISTDIR}/PIC32MX250F128B/package
+CND_PACKAGE_NAME_PIC32MX250F128B=hw8.x.tar
+CND_PACKAGE_PATH_PIC32MX250F128B=${CND_DISTDIR}/PIC32MX250F128B/package/hw8.x.tar
