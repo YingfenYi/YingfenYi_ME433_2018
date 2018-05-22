f885358317d8b6c3d52586944e3023654698ed82
Yingfen Yi <32470088+YingfenYi@users.noreply.github.com>
2018-05-22 15:09:26 -0500

    hw1 slightly change

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -1,41 +1,41 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 
+
 // DEVCFG0
-#pragma config DEBUG = 0b10 // no debugging
-#pragma config JTAGEN = 0 // no jtag
-#pragma config ICESEL = 0b11 // use PGED1 and PGEC1
-#pragma config PWP = 0x1FF // no write protect
-#pragma config BWP = 1 // no boot write protect
-#pragma config CP = 1 // no code protect
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 0b011 // use primary oscillator with pll
-#pragma config FSOSCEN = 0 // turn off secondary oscillator
-#pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 0b10 // high speed crystal mode
-#pragma config OSCIOFNC = 1 // disable secondary osc
-#pragma config FPBDIV = 0b00 // divide sysclk freq by 1 for peripheral bus clock
-#pragma config FCKSM = 0b11 // do not enable clock switch
-#pragma config WDTPS = 0 // use slowest wdt
-#pragma config WINDIS = 1 // wdt no window mode
-#pragma config FWDTEN = 0 // wdt disabled
-#pragma config FWDTWINSZ = 0b11 // wdt window at 25%
+#pragma config FNOSC = FRCPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576  // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
-#pragma config FPLLIDIV = 1 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = 0b111 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = 1 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = 1 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
-#pragma config UPLLEN = 0 // USB clock on
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz[2x Divider,8->4]
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV[24x,4->96]
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz[Divide by 2,96->48]
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB[8*12/2->48][12multi is unsettable]
+#pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
 #pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
-#pragma config IOL1WAY = 0 // allow multiple reconfigurations
-#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
-#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
-
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 int main() {
 
